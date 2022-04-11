e74ebddea82be29745d8a1d939c2bd02204a0ec7
lauripalonen <palonen.lauri@gmail.com>
2022-04-11 09:15:52 +0300

    apply csrf vulnerability

--- csbproject/csbproject/settings.py
+++ csbproject/csbproject/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'usermgmnt.apps.UsermgmntConfig',
     'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- csbproject/polls/views.py
+++ csbproject/polls/views.py
@@ -4,12 +4,14 @@ from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
 
+from django.views.decorators.csrf import csrf_exempt
+
 from .models import Choice, Question
+
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
-
     def get_queryset(self):
         """
         Return the last five published questions (not including those set to be
@@ -22,6 +24,7 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
     def get_queryset(self):
         """
         Excludes any questions that aren't published yet.
@@ -32,6 +35,7 @@ class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
 
+@csrf_exempt
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
@@ -45,7 +49,4 @@ def vote(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
